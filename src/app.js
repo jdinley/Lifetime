5d1907e34d8378c5376388e9490ef4e098324db4
jdinley <jdinley@gmail.com>
2019-04-20 11:19:44 -0400

    Setting up project with Parcel.

---
+++ src/app.js
@@ -0,0 +1,35 @@
+new Vue({
+	el: ".container",
+
+	data: {
+		age: "45",
+		life: "82",
+		show: false,
+		ageSize: true,
+		months: 52.143
+	},
+
+	computed: {
+		weeksAge: function() {
+			return Math.round(parseInt(this.age) * this.months);
+		},
+		weeksLife: function() {
+			return Math.round(parseInt(this.life) * 52.143 - this.weeksAge);
+		},
+		weeksTotal: function() {
+			return Math.round(parseInt(this.life) * 52.143);
+		},
+		weeksRemaining: function() {
+			return this.weeksTotal - this.weeksAge;
+		},
+		yearsRemaining: function() {
+			return this.life - this.age;
+		},
+		monthsInYear: function() {
+			return Math.round(parseInt(this.age) * 12);
+		},
+		ageLength: function() {
+			return this.age.length;
+		}
+	}
+});
